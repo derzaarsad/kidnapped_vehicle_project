7d6f917b2e8dc21f7b22027cd05fe6f6e876c123
derzaarsad <derza.arsad@yahoo.com>
2019-02-05 23:48:26 +0100

    init and predicted implemented

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -30,7 +30,28 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  num_particles = 1000;  // TODO: Set the number of particles
+
+  std::default_random_engine gen;
+  double std_x, std_y, std_theta;  // Standard deviations for x, y, and theta
+  std_x = std[0];
+  std_y = std[1];
+  std_theta = std[2];
+
+  // This line creates a normal (Gaussian) distribution for x
+  std::normal_distribution<double> dist_x(x, std_x);
+  std::normal_distribution<double> dist_y(y, std_y);
+  std::normal_distribution<double> dist_theta(theta, std_theta);
+
+  for (int i = 0; i < num_particles; ++i) {
+    Particle p;
+    p.id = i;
+    p.x = dist_x(gen);
+    p.y = dist_y(gen);
+    p.theta = dist_theta(gen);
+    p.weight = 1;
+    particles.push_back(p);
+  }
 
 }
 
@@ -43,7 +64,25 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
    *  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
    *  http://www.cplusplus.com/reference/random/default_random_engine/
    */
+  std::default_random_engine gen;
+  double std_x, std_y, std_theta;  // Standard deviations for x, y, and theta
+  std_x = std_pos[0];
+  std_y = std_pos[1];
+  std_theta = std_pos[2];
 
+  for (int i = 0; i < num_particles; ++i) {
+
+    double pred_theta = particles[i].theta + yaw_rate * delta_t; // normalized?
+    double pred_x = particles[i].x + velocity / yaw_rate * (sin(pred_theta) - sin(particles[i].theta));
+    double pred_y = particles[i].y + velocity / yaw_rate * (cos(particles[i].theta) - cos(pred_theta));
+    std::normal_distribution<double> dist_x(pred_x, std_x);
+    std::normal_distribution<double> dist_y(pred_y, std_y);
+    std::normal_distribution<double> dist_theta(pred_theta, std_theta);
+
+    particles[i].x = dist_x(gen);
+    particles[i].y = dist_y(gen);
+    particles[i].theta = dist_theta(gen);
+  }
 }
 
 void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted, 
@@ -75,7 +114,46 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
    *   and the following is a good resource for the actual equation to implement
    *   (look at equation 3.33) http://planning.cs.uiuc.edu/node99.html
    */
+  for (int i = 0; i < num_particles; ++i) {
 
+    double x_part = particles[i].x;
+    double y_part = particles[i].y;
+    double cos_theta = std::cos(particles[i].theta);
+    double sin_theta = std::sin(particles[i].theta);
+
+    // computes the inverse of a transformation matrix
+    double det = cos_theta * cos_theta +
+                 sin_theta * sin_theta;
+
+    double invdet = 1 / det;
+
+    double minv00 = cos_theta * invdet;
+    double minv01 =  sin_theta * invdet;
+    double minv02 = (-sin_theta * y_part - x_part * cos_theta) * invdet;
+    double minv10 =  -sin_theta * invdet;
+    double minv11 = cos_theta * invdet;
+    double minv12 = (sin_theta * x_part - cos_theta * y_part) * invdet;
+
+    std::vector<LandmarkObs> mapInVeh;
+    for (auto& map_landmark : map_landmarks.landmark_list) {
+      double distance = std::sqrt(std::pow(x_part - map_landmark.x_f, 2) + std::pow(y_part - map_landmark.y_f, 2));
+      if(distance <= sensor_range) {
+        // transform to VEHICLE'S coordinate system
+        double x_map_in_veh = minv02 + (minv00 * map_landmark.x_f) + (minv01 * map_landmark.y_f);
+        double y_map_in_veh = minv12 + (minv10 * map_landmark.x_f) + (minv11 * map_landmark.y_f);
+        LandmarkObs landmark;
+        landmark.id = map_landmark.id_i;
+        landmark.x = x_map_in_veh;
+        landmark.y = y_map_in_veh;
+        mapInVeh.push_back(landmark);
+        // HOW ABOUT STANDARD DEVIATION?????
+      }
+    }
+
+    vector<LandmarkObs> observations_ = observations;
+    dataAssociation(mapInVeh, observations_);
+
+  }
 }
 
 void ParticleFilter::resample() {
